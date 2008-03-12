joker-eph/clangllvm

commit 7a0b83ab4dd5e6b411e30c28222382426a6add91

Prototype (pre-alpha) implementation of CFRef checker.
git-svn-id: https://llvm.org/svn/llvm-project@48272 91177308-0d34-0410-b5e6-96231b3b80d8

cfe/trunk/include/clang/Analysis/PathSensitive/SymbolManager.h
@@ -20,7 +20,7 @@
 #include "llvm/ADT/FoldingSet.h"
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/Support/DataTypes.h"
-#include <vector>
+#include "llvm/Support/Allocator.h"
 
 namespace clang {
   
@@ -28,15 +28,14 @@ class SymbolManager;
   
 class SymbolID {
   unsigned Data;
+  
 public:
-  SymbolID() : Data(~0) {}
   SymbolID(unsigned x) : Data(x) {}
   
-  bool isInitialized() const { return Data != (unsigned) ~0; }
-  operator unsigned() const { assert (isInitialized()); return Data; }
+  operator unsigned() const { return Data; }
+  unsigned getNumber() const { return Data; }
   
   void Profile(llvm::FoldingSetNodeID& ID) const { 
-    assert (isInitialized());
     ID.AddInteger(Data);
   }
   
@@ -44,51 +43,74 @@ public:
     X.Profile(ID);
   }
 };
+  
+} // end clang namespace
+
+namespace llvm {
+  template <> struct DenseMapInfo<clang::SymbolID> {
+    static inline clang::SymbolID getEmptyKey() {
+      return clang::SymbolID(~0U);
+    }
+    static inline clang::SymbolID getTombstoneKey() {
+      return clang::SymbolID(~0U - 1);
+    }
+    static unsigned getHashValue(clang::SymbolID X) {
+      return X.getNumber();
+    }
+    static bool isEqual(clang::SymbolID X, clang::SymbolID Y) {
+      return X.getNumber() == Y.getNumber();
+    }
+    static bool isPod() { return true; }
+  };
+}
 
 // SymbolData: Used to record meta data about symbols.
 
-class SymbolData {
+namespace clang {
+  
+class SymbolData : public llvm::FoldingSetNode {
 public:
-  enum Kind { UndefKind, ParmKind, GlobalKind, ContentsOfKind };
+  enum Kind { UndefKind, ParmKind, GlobalKind, ContentsOfKind, CallRetValKind };
   
 private:
-  uintptr_t Data;
   Kind K;
+  SymbolID Sym;
   
 protected:
-  SymbolData(uintptr_t D, Kind k) : Data(D), K(k) {}
-  SymbolData(void* D, Kind k) : Data(reinterpret_cast<uintptr_t>(D)), K(k) {}
-  
-  void* getPtr() const { 
-    assert (K != UndefKind);
-    return reinterpret_cast<void*>(Data);
-  }
-  
-  uintptr_t getInt() const {
-    assert (K != UndefKind);
-    return Data;
-  }
-  
+  SymbolData(Kind k, SymbolID sym) : K(k), Sym(sym) {}  
+
 public:
-  SymbolData() : Data(0), K(UndefKind) {}
+  virtual ~SymbolData() {}
   
-  Kind  getKind() const { return K; }  
+  Kind getKind() const { return K; }  
   
-  inline bool operator==(const SymbolData& R) const { 
-    return K == R.K && Data == R.Data;
-  }
-  
+  SymbolID getSymbol() const { return Sym; }
+    
   QualType getType(const SymbolManager& SymMgr) const;
+  
+  virtual void Profile(llvm::FoldingSetNodeID& profile) = 0;
   
   // Implement isa<T> support.
   static inline bool classof(const SymbolData*) { return true; }
 };
 
 class SymbolDataParmVar : public SymbolData {
-public:
-  SymbolDataParmVar(ParmVarDecl* VD) : SymbolData(VD, ParmKind) {}
+  ParmVarDecl *VD;
+
+public:  
+  SymbolDataParmVar(SymbolID MySym, ParmVarDecl* vd)
+    : SymbolData(ParmKind, MySym), VD(vd) {}
   
-  ParmVarDecl* getDecl() const { return (ParmVarDecl*) getPtr(); }
+  ParmVarDecl* getDecl() const { return VD; }  
+  
+  static void Profile(llvm::FoldingSetNodeID& profile, ParmVarDecl* VD) {
+    profile.AddInteger((unsigned) ParmKind);
+    profile.AddPointer(VD);
+  }
+  
+  virtual void Profile(llvm::FoldingSetNodeID& profile) {
+    Profile(profile, VD);
+  }
   
   // Implement isa<T> support.
   static inline bool classof(const SymbolData* D) {
@@ -97,10 +119,22 @@ public:
 };
   
 class SymbolDataGlobalVar : public SymbolData {
+  VarDecl *VD;
+
 public:
-  SymbolDataGlobalVar(VarDecl* VD) : SymbolData(VD, GlobalKind) {}
+  SymbolDataGlobalVar(SymbolID MySym, VarDecl* vd) :
+    SymbolData(GlobalKind, MySym), VD(vd) {}
   
-  VarDecl* getDecl() const { return (VarDecl*) getPtr(); }
+  VarDecl* getDecl() const { return VD; }
+  
+  static void Profile(llvm::FoldingSetNodeID& profile, VarDecl* VD) {
+    profile.AddInteger((unsigned) GlobalKind);
+    profile.AddPointer(VD);
+  }
+  
+  virtual void Profile(llvm::FoldingSetNodeID& profile) {
+    Profile(profile, VD);
+  }
   
   // Implement isa<T> support.
   static inline bool classof(const SymbolData* D) {
@@ -109,14 +143,55 @@ public:
 };
 
 class SymbolDataContentsOf : public SymbolData {
+  SymbolID Sym;
+      
 public:
-  SymbolDataContentsOf(SymbolID ID) : SymbolData(ID, ContentsOfKind) {}
+  SymbolDataContentsOf(SymbolID MySym, SymbolID sym) : 
+    SymbolData(ContentsOfKind, MySym), Sym(sym) {}
   
-  SymbolID getSymbol() const { return (SymbolID) getInt(); }
+  SymbolID getContainerSymbol() const { return Sym; }
+  
+  static void Profile(llvm::FoldingSetNodeID& profile, SymbolID Sym) {
+    profile.AddInteger((unsigned) ContentsOfKind);
+    profile.AddInteger(Sym);
+  }
+  
+  virtual void Profile(llvm::FoldingSetNodeID& profile) {
+    Profile(profile, Sym);
+  }
   
   // Implement isa<T> support.
   static inline bool classof(const SymbolData* D) {
     return D->getKind() == ContentsOfKind;
+  }  
+};
+  
+class SymbolDataCallRetVal : public SymbolData {
+  CallExpr* CE;
+  unsigned Count;
+
+public:
+  SymbolDataCallRetVal(SymbolID Sym, CallExpr* ce, unsigned count)
+    : SymbolData(CallRetValKind, Sym), CE(ce), Count(count) {}
+  
+  CallExpr* getCallExpr() const { return CE; }
+  unsigned getCount() const { return Count; }  
+  
+  static void Profile(llvm::FoldingSetNodeID& profile,
+                      CallExpr* CE, unsigned Count) {
+    
+    profile.AddInteger((unsigned) CallRetValKind);
+    profile.AddPointer(CE);
+    profile.AddInteger(Count);
+  }
+  
+  virtual void Profile(llvm::FoldingSetNodeID& profile) {
+    Profile(profile, CE, Count);
+  }
+  
+  // Implement isa<T> support.
+  static inline bool classof(const SymbolData* D) {
+    return D->getKind() == CallRetValKind;
   }  
 };
 
@@ -133,7 +208,7 @@ public:
   Op(op), Val(V) {}
   
   BinaryOperator::Opcode getOpcode() const { return Op; }
-  const SymbolID& getSymbol() const { return Symbol; }
+  SymbolID getSymbol() const { return Symbol; }
   const llvm::APSInt& getInt() const { return Val; }
   
   static inline void Profile(llvm::FoldingSetNodeID& ID,
@@ -152,32 +227,28 @@ public:
 
 
 class SymbolManager {
-  std::vector<SymbolData> SymbolToData;
+  typedef llvm::FoldingSet<SymbolData> DataSetTy;
+  typedef llvm::DenseMap<SymbolID, SymbolData*> DataMapTy;
   
-  typedef llvm::DenseMap<void*,SymbolID> MapTy;
-  MapTy DataToSymbol;
+  DataSetTy DataSet;
+  DataMapTy DataMap;
   
-  void* getKey(void* P) const {
-    return reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(P) | 0x1);
-  }
-  
-  void* getKey(SymbolID sym) const {
-    return reinterpret_cast<void*>((uintptr_t) (sym << 1));
-  }
+  unsigned SymbolCounter;
+  llvm::BumpPtrAllocator& BPAlloc;
   
 public:
-  SymbolManager();
+  SymbolManager(llvm::BumpPtrAllocator& bpalloc)
+    : SymbolCounter(0), BPAlloc(bpalloc) {}
+  
   ~SymbolManager();
   
   SymbolID getSymbol(VarDecl* D);
   SymbolID getContentsOfSymbol(SymbolID sym);
+  SymbolID getCallRetValSymbol(CallExpr* CE, unsigned VisitCount);
   
-  inline const SymbolData& getSymbolData(SymbolID ID) const {
-    assert (ID < SymbolToData.size());
-    return SymbolToData[ID];
-  }
+  const SymbolData& getSymbolData(SymbolID ID) const;
   
-  inline QualType getType(SymbolID ID) const {
+  QualType getType(SymbolID ID) const {
     return getSymbolData(ID).getType(*this);
   }
 };

cfe/trunk/Analysis/CFRefCount.cpp
@@ -86,6 +86,10 @@ public:
     return (*Args)[idx];
   }
   
+  RetEffect getRet() const {
+    return Ret;
+  }
+  
   typedef ArgEffects::const_iterator arg_iterator;
   
   arg_iterator begin_args() const { return Args->begin(); }
@@ -113,12 +117,24 @@ class CFRefSummaryManager {
   llvm::BumpPtrAllocator BPAlloc;
   
   ArgEffects             ScratchArgs;
+  
+  
+  ArgEffects*   getArgEffects();
 
+  CFRefSummary* getCannedCFSummary(FunctionTypeProto* FT, bool isRetain);
+
+  CFRefSummary* getCFSummary(FunctionDecl* FD, const char* FName);
+  
+  CFRefSummary* getCFSummaryCreateRule(FunctionTypeProto* FT);
+  CFRefSummary* getCFSummaryGetRule(FunctionTypeProto* FT);  
+  
+  CFRefSummary* getPersistentSummary(ArgEffects* AE, RetEffect RE);
+  
 public:
   CFRefSummaryManager() {}
   ~CFRefSummaryManager();
   
-  CFRefSummary* getSummary(FunctionDecl* FD);
+  CFRefSummary* getSummary(FunctionDecl* FD, ASTContext& Ctx);
 };
   
 } // end anonymous namespace
@@ -137,7 +153,59 @@ CFRefSummaryManager::~CFRefSummaryManager() {
     I->getValue().~ArgEffects();
 }
 
-CFRefSummary* CFRefSummaryManager::getSummary(FunctionDecl* FD) {
+ArgEffects* CFRefSummaryManager::getArgEffects() {
+
+  assert (!ScratchArgs.empty());
+  
+  llvm::FoldingSetNodeID profile;
+  profile.Add(ScratchArgs);
+  void* InsertPos;
+  
+  llvm::FoldingSetNodeWrapper<ArgEffects>* E =
+    AESet.FindNodeOrInsertPos(profile, InsertPos);
+  
+  if (E) {    
+    ScratchArgs.clear();
+    return &E->getValue();
+  }
+  
+  E = (llvm::FoldingSetNodeWrapper<ArgEffects>*)
+      BPAlloc.Allocate<llvm::FoldingSetNodeWrapper<ArgEffects> >();
+                       
+  new (E) llvm::FoldingSetNodeWrapper<ArgEffects>(ScratchArgs);
+  AESet.InsertNode(E, InsertPos);
+
+  ScratchArgs.clear();
+  return &E->getValue();
+}
+
+CFRefSummary* CFRefSummaryManager::getPersistentSummary(ArgEffects* AE,
+                                                        RetEffect RE) {
+  
+  llvm::FoldingSetNodeID profile;
+  CFRefSummary::Profile(profile, AE, RE);
+  void* InsertPos;
+  
+  CFRefSummary* Summ = SummarySet.FindNodeOrInsertPos(profile, InsertPos);
+  
+  if (Summ)
+    return Summ;
+  
+  Summ = (CFRefSummary*) BPAlloc.Allocate<CFRefSummary>();
+  new (Summ) CFRefSummary(AE, RE);
+  SummarySet.InsertNode(Summ, InsertPos);
+  
+  return Summ;
+}
+
+
+CFRefSummary* CFRefSummaryManager::getSummary(FunctionDecl* FD,
+                                              ASTContext& Ctx) {
+
+  SourceLocation Loc = FD->getLocation();
+  
+  if (!Loc.isFileID())
+    return NULL;
   
   { // Look into our cache of summaries to see if we have already computed
     // a summary for this FunctionDecl.
@@ -148,10 +216,167 @@ CFRefSummary* CFRefSummaryManager::getSummary(FunctionDecl* FD) {
       return I->second;
   }
   
-  //
+#if 0
+  SourceManager& SrcMgr = Ctx.getSourceManager();
+  unsigned fid = Loc.getFileID();
+  const FileEntry* FE = SrcMgr.getFileEntryForID(fid);
   
+  if (!FE)
+    return NULL;
+  
+  const char* DirName = FE->getDir()->getName();  
+  assert (DirName);
+  assert (strlen(DirName) > 0);
+  
+  if (!strstr(DirName, "CoreFoundation")) {
+    SummaryMap[FD] = NULL;
+    return NULL;
+  }
+#endif
+  
+  const char* FName = FD->getIdentifier()->getName();
+    
+  if (FName[0] == 'C' && FName[1] == 'F') {
+    CFRefSummary* S = getCFSummary(FD, FName);
+    SummaryMap[FD] = S;
+    return S;
+  }
   
   return NULL;  
+}
+
+CFRefSummary* CFRefSummaryManager::getCFSummary(FunctionDecl* FD,
+                                                const char* FName) {
+  
+  // For now, only generate summaries for functions that have a prototype.
+  
+  FunctionTypeProto* FT =
+    dyn_cast<FunctionTypeProto>(FD->getType().getTypePtr());
+  
+  if (!FT)
+    return NULL;
+  
+  FName += 2;
+
+  if (strcmp(FName, "Retain") == 0)
+    return getCannedCFSummary(FT, true);
+  
+  if (strcmp(FName, "Release") == 0)
+    return getCannedCFSummary(FT, false);
+  
+  assert (ScratchArgs.empty());
+  bool usesCreateRule = false;
+  
+  if (strstr(FName, "Create"))
+    usesCreateRule = true;
+  
+  if (!usesCreateRule && strstr(FName, "Copy"))
+    usesCreateRule = true;
+  
+  if (usesCreateRule)
+    return getCFSummaryCreateRule(FT);
+
+  if (strstr(FName, "Get"))
+    return getCFSummaryGetRule(FT);
+  
+  return NULL;
+}
+
+CFRefSummary* CFRefSummaryManager::getCannedCFSummary(FunctionTypeProto* FT,
+                                                      bool isRetain) {
+  
+  if (FT->getNumArgs() != 1)
+    return NULL;
+  
+  TypedefType* ArgT = dyn_cast<TypedefType>(FT->getArgType(0).getTypePtr());
+  
+  if (!ArgT)
+    return NULL;
+  
+  // For CFRetain/CFRelease, the first (and only) argument is of type 
+  // "CFTypeRef".
+  
+  const char* TDName = ArgT->getDecl()->getIdentifier()->getName();
+  assert (TDName);
+  
+  if (strcmp("CFTypeRef", TDName) == 0)
+    return NULL;
+  
+  if (!ArgT->isPointerType())
+    return NULL;
+  
+  // Check the return type.  It should also be "CFTypeRef".
+  
+  QualType RetTy = FT->getResultType();
+  
+  if (RetTy.getTypePtr() != ArgT)
+    return NULL;
+  
+  // The function's interface checks out.  Generate a canned summary.
+  
+  assert (ScratchArgs.empty());
+  ScratchArgs.push_back(isRetain ? IncRef : DecRef);
+  
+  return getPersistentSummary(getArgEffects(), RetEffect::MakeAlias(0));
+}
+
+static bool isCFRefType(QualType T) {
+  
+  if (!T->isPointerType())
+    return false;
+  
+  // Check the typedef for the name "CF" and the substring "Ref".
+  
+  TypedefType* TD = dyn_cast<TypedefType>(T.getTypePtr());
+  
+  if (!TD)
+    return false;
+  
+  const char* TDName = TD->getDecl()->getIdentifier()->getName();
+  assert (TDName);
+  
+  if (TDName[0] != 'C' || TDName[1] != 'F')
+    return false;
+  
+  if (strstr(TDName, "Ref") == 0)
+    return false;
+  
+  return true;
+}
+  
+
+CFRefSummary*
+CFRefSummaryManager::getCFSummaryCreateRule(FunctionTypeProto* FT) {
+ 
+  if (!isCFRefType(FT->getResultType()))
+    return NULL;
+
+  assert (ScratchArgs.empty());
+  
+  // FIXME: Add special-cases for functions that retain/release.  For now
+  //  just handle the default case.
+  
+  for (unsigned i = 0, n = FT->getNumArgs(); i != n; ++i)
+    ScratchArgs.push_back(DoNothing);
+  
+  return getPersistentSummary(getArgEffects(), RetEffect::MakeOwned());
+}
+
+CFRefSummary*
+CFRefSummaryManager::getCFSummaryGetRule(FunctionTypeProto* FT) {
+  
+  if (!isCFRefType(FT->getResultType()))
+    return NULL;
+  
+  assert (ScratchArgs.empty());
+  
+  // FIXME: Add special-cases for functions that retain/release.  For now
+  //  just handle the default case.
+  
+  for (unsigned i = 0, n = FT->getNumArgs(); i != n; ++i)
+    ScratchArgs.push_back(DoNothing);
+  
+  return getPersistentSummary(getArgEffects(), RetEffect::MakeNotOwned());
 }
 
 //===----------------------------------------------------------------------===//
@@ -246,8 +471,8 @@ class CFRefCount : public GRSimpleVals {
   // Instance variables.
   
   CFRefSummaryManager Summaries;
-  RefBFactoryTy  RefBFactory;
-  
+  RefBFactoryTy       RefBFactory;
+     
   UseAfterReleasesTy UseAfterReleases;
   ReleasesNotOwnedTy ReleasesNotOwned;
   
@@ -282,9 +507,8 @@ public:
   // Calls.
   
   virtual void EvalCall(ExplodedNodeSet<ValueState>& Dst,
-                        ValueStateManager& StateMgr,
+                        GRExprEngine& Engine,
                         GRStmtNodeBuilder<ValueState>& Builder,
-                        BasicValueFactory& BasicVals,
                         CallExpr* CE, LVal L,
                         ExplodedNode<ValueState>* Pred);  
 };
@@ -307,11 +531,12 @@ void CFRefCount::BindingsPrinter::PrintCheckerState(std::ostream& Out,
 }
 
 void CFRefCount::EvalCall(ExplodedNodeSet<ValueState>& Dst,
-                            ValueStateManager& StateMgr,
-                            GRStmtNodeBuilder<ValueState>& Builder,
-                            BasicValueFactory& BasicVals,
-                            CallExpr* CE, LVal L,
-                            ExplodedNode<ValueState>* Pred) {
+                          GRExprEngine& Engine,
+                          GRStmtNodeBuilder<ValueState>& Builder,
+                          CallExpr* CE, LVal L,
+                          ExplodedNode<ValueState>* Pred) {
+  
+  ValueStateManager& StateMgr = Engine.getStateManager();
   
   // FIXME: Support calls to things other than lval::FuncVal.  At the very
   //  least we should stop tracking ref-state for ref-counted objects passed
@@ -323,7 +548,7 @@ void CFRefCount::EvalCall(ExplodedNodeSet<ValueState>& Dst,
 
   lval::FuncVal FV = cast<lval::FuncVal>(L);
   FunctionDecl* FD = FV.getDecl();
-  CFRefSummary* Summ = Summaries.getSummary(FD);
+  CFRefSummary* Summ = Summaries.getSummary(FD, Engine.getContext());
 
   // Get the state.
   
@@ -355,35 +580,36 @@ void CFRefCount::EvalCall(ExplodedNodeSet<ValueState>& Dst,
             
       if (isa<LVal>(V))
         StateMgr.Unbind(StVals, cast<LVal>(V));
-    }    
+    }
+    
+    St = StateMgr.getPersistentState(StVals);
+    Builder.Nodify(Dst, CE, Pred, St);
+    return;
   }
-  else {
+  
+  // This function has a summary.  Evaluate the effect of the arguments.
+  
+  unsigned idx = 0;
+  
+  for (CallExpr::arg_iterator I=CE->arg_begin(), E=CE->arg_end();
+        I!=E; ++I, ++idx) {
     
-    // This function has a summary.  Evaluate the effect of the arguments.
+    RVal V = StateMgr.GetRVal(St, *I);
     
-    unsigned idx = 0;
-    
-    for (CallExpr::arg_iterator I=CE->arg_begin(), E=CE->arg_end();
-          I!=E; ++I, ++idx) {
-      
-      RVal V = StateMgr.GetRVal(St, *I);
-      
-      if (isa<lval::SymbolVal>(V)) {
-        SymbolID Sym = cast<lval::SymbolVal>(V).getSymbol();
-        RefBindings B = GetRefBindings(StVals);
+    if (isa<lval::SymbolVal>(V)) {
+      SymbolID Sym = cast<lval::SymbolVal>(V).getSymbol();
+      RefBindings B = GetRefBindings(StVals);
 
-        if (RefBindings::TreeTy* T = B.SlimFind(Sym)) {
-          B = Update(B, Sym, T->getValue().second, Summ->getArg(idx), hasError);
-          SetRefBindings(StVals, B);
-          if (hasError) break;
-        }
+      if (RefBindings::TreeTy* T = B.SlimFind(Sym)) {
+        B = Update(B, Sym, T->getValue().second, Summ->getArg(idx), hasError);
+        SetRefBindings(StVals, B);
+        if (hasError) break;
       }
-    }    
-  }
-  
-  St = StateMgr.getPersistentState(StVals);
-  
+    }
+  }    
+    
   if (hasError) {
+    St = StateMgr.getPersistentState(StVals);
     GRExprEngine::NodeTy* N = Builder.generateNode(CE, St, Pred);
 
     if (N) {
@@ -399,10 +625,61 @@ void CFRefCount::EvalCall(ExplodedNodeSet<ValueState>& Dst,
           ReleasesNotOwned.insert(N);
           break;
       }
-    }    
+    }
+    
+    return;
   }
-  else
-    Builder.Nodify(Dst, CE, Pred, St);
+
+  // Finally, consult the summary for the return value.
+  
+  RetEffect RE = Summ->getRet();
+  St = StateMgr.getPersistentState(StVals);
+
+  
+  switch (RE.getKind()) {
+    default:
+      assert (false && "Unhandled RetEffect."); break;
+    
+    case RetEffect::Alias: {
+      unsigned idx = RE.getValue();
+      assert (idx < CE->getNumArgs());
+      RVal V = StateMgr.GetRVal(St, CE->getArg(idx));
+      St = StateMgr.SetRVal(St, CE, V, Engine.getCFG().isBlkExpr(CE), false);
+      break;
+    }
+      
+    case RetEffect::OwnedSymbol: {
+      unsigned Count = Builder.getCurrentBlockCount();
+      SymbolID Sym = Engine.getSymbolManager().getCallRetValSymbol(CE, Count);
+
+      ValueState StImpl = *St;
+      RefBindings B = GetRefBindings(StImpl);
+      SetRefBindings(StImpl, RefBFactory.Add(B, Sym, RefVal::makeOwned(1)));
+      
+      St = StateMgr.SetRVal(StateMgr.getPersistentState(StImpl),
+                            CE, lval::SymbolVal(Sym),
+                            Engine.getCFG().isBlkExpr(CE), false);
+      
+      break;
+    }
+      
+    case RetEffect::NotOwnedSymbol: {
+      unsigned Count = Builder.getCurrentBlockCount();
+      SymbolID Sym = Engine.getSymbolManager().getCallRetValSymbol(CE, Count);
+      
+      ValueState StImpl = *St;
+      RefBindings B = GetRefBindings(StImpl);
+      SetRefBindings(StImpl, RefBFactory.Add(B, Sym, RefVal::makeNotOwned()));
+      
+      St = StateMgr.SetRVal(StateMgr.getPersistentState(StImpl),
+                            CE, lval::SymbolVal(Sym),
+                            Engine.getCFG().isBlkExpr(CE), false);
+      
+      break;
+    }
+  }
+      
+  Builder.Nodify(Dst, CE, Pred, St);
 }
 
 
@@ -418,6 +695,12 @@ CFRefCount::RefBindings CFRefCount::Update(RefBindings B, SymbolID sym,
       assert (false && "Unhandled CFRef transition.");
       
     case DoNothing:
+      if (V.getKind() == RefVal::Released) {
+        V = RefVal::makeUseAfterRelease();        
+        hasError = V.getKind();
+        break;
+      }
+      
       return B;
       
     case IncRef:      

commit 094e6647d5a81cfda26685b41496f2f9e0319d04

Fixed regression in SymbolManager introduced in r48272.
git-svn-id: https://llvm.org/svn/llvm-project@48273 91177308-0d34-0410-b5e6-96231b3b80d8

cfe/trunk/include/clang/Analysis/PathSensitive/SymbolManager.h
@@ -24,23 +24,22 @@
 
 namespace clang {
   
+
 class SymbolManager;
-  
+
 class SymbolID {
   unsigned Data;
-  
 public:
+  SymbolID() : Data(~0U - 2) {}
   SymbolID(unsigned x) : Data(x) {}
-  
-  operator unsigned() const { return Data; }
-  unsigned getNumber() const { return Data; }
-  
+    
+  bool isInitialized() const { return Data != (unsigned) (~0U - 2); }
+  operator unsigned() const { return getNumber(); }
+  unsigned getNumber() const { assert (isInitialized()); return Data; }
+    
   void Profile(llvm::FoldingSetNodeID& ID) const { 
+    assert (isInitialized());
     ID.AddInteger(Data);
-  }
-  
-  static inline void Profile(llvm::FoldingSetNodeID& ID, SymbolID X) {
-    X.Profile(ID);
   }
 };
   
@@ -208,11 +207,11 @@ public:
   Op(op), Val(V) {}
   
   BinaryOperator::Opcode getOpcode() const { return Op; }
-  SymbolID getSymbol() const { return Symbol; }
+  const SymbolID& getSymbol() const { return Symbol; }
   const llvm::APSInt& getInt() const { return Val; }
   
   static inline void Profile(llvm::FoldingSetNodeID& ID,
-                             const SymbolID& Symbol,
+                             SymbolID Symbol,
                              BinaryOperator::Opcode Op,
                              const llvm::APSInt& Val) {
     Symbol.Profile(ID);