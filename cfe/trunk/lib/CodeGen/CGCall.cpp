joker-eph/clangllvm

commit 1b3db1494a0995153fbf9402eea553810e86fe25

Use getDebugLoc and setDebugLoc instead of getDbgMetadata and setDbgMetadata, avoiding MDNode overhead.
git-svn-id: https://llvm.org/svn/llvm-project@108911 91177308-0d34-0410-b5e6-96231b3b80d8

cfe/trunk/lib/CodeGen/CGCall.cpp
@@ -1000,7 +1000,7 @@ void CodeGenFunction::EmitFunctionEpilog(const CGFunctionInfo &FI) {
     return;
   }
 
-  llvm::MDNode *RetDbgInfo = 0;
+  llvm::DebugLoc RetDbgLoc;
   llvm::Value *RV = 0;
   QualType RetTy = FI.getReturnType();
   const ABIArgInfo &RetAI = FI.getReturnInfo();
@@ -1034,7 +1034,7 @@ void CodeGenFunction::EmitFunctionEpilog(const CGFunctionInfo &FI) {
       RV = Builder.CreateLoad(ReturnValue);
     } else {
       // Get the stored value and nuke the now-dead store.
-      RetDbgInfo = SI->getDbgMetadata();
+      RetDbgLoc = SI->getDebugLoc();
       RV = SI->getValueOperand();
       SI->eraseFromParent();
       
@@ -1058,8 +1058,7 @@ void CodeGenFunction::EmitFunctionEpilog(const CGFunctionInfo &FI) {
   }
 
   llvm::Instruction *Ret = RV ? Builder.CreateRet(RV) : Builder.CreateRetVoid();
-  if (RetDbgInfo)
-    Ret->setDbgMetadata(RetDbgInfo);
+  Ret->setDebugLoc(RetDbgLoc);
 }
 
 RValue CodeGenFunction::EmitDelegateCallArg(const VarDecl *Param) {

commit 01e579f4a5e47b2402841de4026543826d1e7a52

Fix regression caused by r108911. Do not override known debug loc with unknown debug loc. This is tested by sections.exp in gdb testsuite.
git-svn-id: https://llvm.org/svn/llvm-project@109022 91177308-0d34-0410-b5e6-96231b3b80d8

cfe/trunk/lib/CodeGen/CGCall.cpp
@@ -1058,7 +1058,8 @@ void CodeGenFunction::EmitFunctionEpilog(const CGFunctionInfo &FI) {
   }
 
   llvm::Instruction *Ret = RV ? Builder.CreateRet(RV) : Builder.CreateRetVoid();
-  Ret->setDebugLoc(RetDbgLoc);
+  if (!RetDbgLoc.isUnknown())
+    Ret->setDebugLoc(RetDbgLoc);
 }
 
 RValue CodeGenFunction::EmitDelegateCallArg(const VarDecl *Param) {